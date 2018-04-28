aeaf4d99fe7f8138f71d955ccf3a937bc1808377
Jim Conner <c_rennochj@outlook.com>
2018-04-28 05:45:17 -0500

    commiting

--- deploy-JimCoLexCodeHooks.sh
+++ deploy-JimCoLexCodeHooks.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+git commit -a -m 'commiting'
+
 rm JimCoLexCodeHooks.zip
 
 zip -r JimCoLexCodeHooks.zip pymysql agent_assistance.py balance_inquiry.py jimcodb.py lambda_function.py \
